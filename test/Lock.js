fc5d937b8bafd0bae320a3cfc1c6778391f9eff9
mandyval17 <mayurvalecha2603@gmail.com>
2023-06-17 12:03:48 +0530

    made some minor changes

--- test/Lock.js
+++ test/Lock.js
@@ -1,71 +1,50 @@
+const { expect } = require("chai")
+// const {ethers}=require("hardhat")
 
-const { expect } = require("chai");
-// const { ethers } = require("hardhat");
-// const { ethers } = require("hardhat");
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
 
-// const tokens = (n) => {
-//   return n*(1 ether)
-//   console.log(n.toString())
-// }
-// try {
-  const { ethers } = require("hardhat");
-  const tokens = (n) => {
-    return ethers.utils.parseUnits(n.toString(), 'ether')
-  }
+// Global constants for listing an item...
+const ID = 1
+const NAME = "Shoes"
+const CATEGORY = "Clothing"
+const IMAGE = "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg"
+const COST = tokens(1)
+const RATING = 4
+const STOCK = 5
 
-  
-  // } catch (error) {
-    //   console.log(error)
-    // }
-    // tokens(2)
-    const ID = 1
-    const NAME = "Shoes"
-    const CATEGORY = "Clothing"
-    const IMAGE = "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg"
-    const COST = tokens(3)
-    const RATING = 4
-    const STOCK = 5
-    
-    describe("Dappazon",()=>{
-      
+describe("Dappazon", () => {
+  let dappazon
+  let deployer, buyer
 
-      
-      let Dappazon;
-      let deployer;
-      let buyer;
-      let dappazon;
-  
-  // const getchainid=async()=>{
-  //   const network = await ethers.getDefaultProvider().getNetwork();
-  //   console.log("Network name=", network.name);
-  //   console.log("Network chain id=", network.chainId);
-  // }
-  // getchainid();
-  beforeEach(async()=>{
-    Dappazon=await ethers.getContractFactory("Dappazon");
-    [deployer,buyer]=await ethers.getSigners();
-   
-    // console.log(deployer,buyer)
-    dappazon=await Dappazon.deploy();
+  beforeEach(async () => {
+    // Setup accounts
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    [deployer, buyer] = await ethers.getSigners()
+    // console.log(buyer)
+
+    // Deploy contract
+    dappazon = await Dappazon.deploy()
   })
 
-  describe("Testing Name",()=>{
-    
-    it('is it owner',async()=>{
+  describe("Deployment", () => {
+    it("Sets the owner", async () => {
       expect(await dappazon.owner()).to.equal(deployer.address)
     })
-
   })
-  describe("Listing",()=>{
 
+  describe("Listing", () => {
     let transaction
 
-    beforeEach(async()=>{
+    beforeEach(async () => {
+      // List a item
       transaction = await dappazon.connect(deployer).list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK)
       await transaction.wait()
+      // console.log(transaction)
     })
 
-    it('Returns Item Attribute',async()=>{
+    it("Returns item attributes", async () => {
       const item = await dappazon.items(ID)
 
       expect(item.id).to.equal(ID)
@@ -77,13 +56,10 @@ const { expect } = require("chai");
       expect(item.stock).to.equal(STOCK)
     })
 
-
-    it('Emit List event',async()=>{
-      expect(transaction).to.emit(dappazon,"List")
+    it("Emits List event", () => {
+      expect(transaction).to.emit(dappazon, "List")
     })
-
   })
-
 
   describe("Buying", () => {
     let transaction
@@ -112,7 +88,7 @@ const { expect } = require("chai");
     })
 
     it("Updates the contract balance", async () => {
-      const result = await dappazon.balanceOf()
+      const result = await ethers.provider.getBalance(dappazon.address)
       expect(result).to.equal(COST)
     })
 
@@ -120,38 +96,35 @@ const { expect } = require("chai");
       expect(transaction).to.emit(dappazon, "Buy")
     })
   })
-  describe("Withdraw", () => {
-    let result;
+
+  describe("Withdrawing", () => {
+    let balanceBefore
 
     beforeEach(async () => {
       // List a item
-      transaction = await dappazon.connect(deployer).list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK)
+      let transaction = await dappazon.connect(deployer).list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK)
       await transaction.wait()
 
       // Buy a item
       transaction = await dappazon.connect(buyer).buy(ID, { value: COST })
       await transaction.wait()
 
-      result = await dappazon.anotherContractBalance(deployer.address)
-      // console.log("result",result)
+      // Get Deployer balance before
+      balanceBefore = await ethers.provider.getBalance(deployer.address)
 
-      transaction=await dappazon.connect(deployer).withdraw();
+      // Withdraw
+      transaction = await dappazon.connect(deployer).withdraw()
       await transaction.wait()
     })
-    
-    it("deployer balance before withdraw and after withdraw", async () => {
-      // console.log(deployer.address)
-      const result2= await dappazon.anotherContractBalance(deployer.address)
-      // console.log("result2",result2)
-      expect(await result2).to.be.greaterThan(await result)
+
+    it('Updates the owner balance', async () => {
+      const balanceAfter = await ethers.provider.getBalance(deployer.address)
+      expect(balanceAfter).to.be.greaterThan(balanceBefore)
     })
 
     it('Updates the contract balance', async () => {
-      const resultt = await dappazon.balanceOf()
-      // console.log((resultt))
-      expect(await resultt).to.equal(0)
+      const result = await ethers.provider.getBalance(dappazon.address)
+      expect(result).to.equal(0)
     })
-
   })
-
 })
